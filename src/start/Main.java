35b4b855f3b57047f7fb4e852cd6ddcab10fd4a2
pyuresha <dariyaarzhenev@gmail.com>
2021-06-01 12:34:59 +0900

    Result

--- src/start/Main.java
+++ src/start/Main.java
@@ -6,13 +6,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner in = new Scanner(System.in);
-
+        System.out.print("Введите количество игроков: ");
         int n = in.nextInt();
 
         Random card = new Random();
-
-
+        card.setNumber_players(n);
+        card.Print();
     }
 }
