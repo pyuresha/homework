ae813eef92ddb3c177038e9b0c7f9a7a74c6e234
pyuresha <dariyaarzhenev@gmail.com>
2021-06-01 12:28:09 +0900

    add_get

--- src/start/Main.java
+++ src/start/Main.java
@@ -1,4 +1,22 @@
 package start;
 
+import java.util.ArrayList;
+
 public class Main {
+    private ArrayList<PlayingCard> deck = new ArrayList<>();
+    private int number_players = 0;
+
+    public int getNumber_players() {
+        return number_players;
+    }
+
+    public void setNumber_players(int number_players) {
+        if (number_players < 10 ) {
+            this.number_players = number_players;
+        }
+        else
+            System.out.println("Не хватит карт, введите от 1 до 10 игроков");
+    }
+
+
 }
