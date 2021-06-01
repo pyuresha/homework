6f07ed6f0b23be6e3ca37859d4193d1aa2c2a69d
pyuresha <dariyaarzhenev@gmail.com>
2021-06-01 12:30:49 +0900

    fix

--- src/start/Random.java
+++ src/start/Random.java
@@ -1,4 +1,34 @@
 package start;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Random {
+    private ArrayList<PlayingCard> deck = new ArrayList<>();
+    private int number_players = 0;
+
+    public int getNumber_players() {
+        return number_players;
+    }
+
+    public void setNumber_players(int number_players) {
+        if (number_players < 10 ) {
+            this.number_players = number_players;
+        }
+        else
+            System.out.println("Не хватит карт, введите от 1 до 10 игроков");
+    }
+
+    public Random()
+    {
+        this.number_players = number_players;
+
+        for (int i = 0; i < PlayingCard.SUITS_LIST.length; i++) {
+            for (int j = 0; j < PlayingCard.RANK_LIST.length; j++) {
+                deck.add(new PlayingCard(PlayingCard.SUITS_LIST[i], PlayingCard.RANK_LIST[j]));
+            }
+        }
+        Collections.shuffle(deck);
+    }
+
 }
