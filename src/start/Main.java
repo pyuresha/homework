aa2b00fb8b5ff6263cd9825f952a419d0a4e63f8
pyuresha <dariyaarzhenev@gmail.com>
2021-06-01 12:34:17 +0900

    main

--- src/start/Main.java
+++ src/start/Main.java
@@ -2,8 +2,14 @@ package start;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 
 public class Main {
+    Scanner in = new Scanner(System.in);
+
+    int n = in.nextInt();
+
+    Random card = new Random();
 
 
 
