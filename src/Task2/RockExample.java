1b78c76d77fb44e5fb1c8f27ab6f1dcbc305b633
pyuresha <dariyaarzhenev@gmail.com>
2021-06-01 13:03:21 +0900

    addGetSet

--- src/Task2/RockExample.java
+++ src/Task2/RockExample.java
@@ -1,15 +1,35 @@
 package Task2;
 
 public class RockExample {
-    /** out
-     *
-     *        *
-     *       ***
-     *       ****   *
-     *     *******  ** ***
-     *    _***************
-     *    0224543211321222
-     */
+    private int maxStep;
+    private int maxDiff;
+    private int route_length;
+
+    public int getMaxStep() {
+        return maxStep;
+    }
+
+    public void setMaxStep(int maxStep) {
+        this.maxStep = maxStep;
+    }
+
+    public int getMaxDiff() {
+        return maxDiff;
+    }
+
+    public void setMaxDiff(int maxDiff) {
+        this.maxDiff = maxDiff;
+    }
+
+    public int getRoute_length() {
+        return route_length;
+    }
+
+    public void setRoute_length(int route_length) {
+        this.route_length = route_length;
+    }
+    
+
     public static final int [] example1 = new int[]{0,2,2,4,5,4,3,2,1,1,3,2,1,2,2,2};
 
     /** out
